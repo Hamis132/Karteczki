4c9e855f37d87b4f742c527cfd13a0443f338f49
Paweł Halicki <pawelhal12@gmail.com>
2017-05-30 00:23:15 +0200

    Dodanie Interface-ow Firmy i Cantrum

---
+++ Centrum_Obslugi_Kart_Platniczych/Centrum_Obslugi_Kart_Platniczych/IFirma.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centrum_Obslugi_Kart_Platniczych
+{
+    interface IFirma : IKlient
+    {
+        ICentrum centrum { get; }
+    }
+}
